ddc200f9c79db7b022a52bcee1ef86c850cea063
Chris Ha <chunghha@users.noreply.github.com>
2020-06-16 03:51:41 -0500

    chore: customized rustfmt

--- src/bin/decode.rs
+++ src/bin/decode.rs
@@ -5,28 +5,28 @@ use std::{collections::HashMap, fs::File};
 
 #[derive(Debug, Deserialize)]
 struct Config {
-    boolean: bool,
-    float: f32,
-    map: HashMap<u8, char>,
-    nested: Nested,
-    tuple: (u32, u32),
+  boolean: bool,
+  float: f32,
+  map: HashMap<u8, char>,
+  nested: Nested,
+  tuple: (u32, u32),
 }
 
 #[derive(Debug, Deserialize)]
 struct Nested {
-    a: String,
-    b: char,
+  a: String,
+  b: char,
 }
 
 fn get_config() -> Result<Config> {
-    let input_path = format!("{}/example.ron", env!("CARGO_MANIFEST_DIR"));
-    let f = File::open(&input_path)?;
-    let config: Config = from_reader(f)?;
+  let input_path = format!("{}/example.ron", env!("CARGO_MANIFEST_DIR"));
+  let f = File::open(&input_path)?;
+  let config: Config = from_reader(f)?;
 
-    Ok(config)
+  Ok(config)
 }
 
 fn main() {
-    better_panic::install();
-    println!("Config: {:#?}", &get_config());
+  better_panic::install();
+  println!("Config: {:#?}", &get_config());
 }
--- src/bin/encode.rs
+++ src/bin/encode.rs
@@ -4,12 +4,12 @@ use std::{collections::HashMap, iter::FromIterator};
 
 #[derive(Serialize)]
 struct Config {
-    float: (f32, f64),
-    tuple: TupleStruct,
-    map: HashMap<u8, char>,
-    nested: Nested,
-    var: Variant,
-    array: Vec<()>,
+  float: (f32, f64),
+  tuple: TupleStruct,
+  map: HashMap<u8, char>,
+  nested: Nested,
+  var: Variant,
+  array: Vec<()>,
 }
 
 #[derive(Serialize)]
@@ -17,33 +17,27 @@ struct TupleStruct((), bool);
 
 #[derive(Serialize)]
 enum Variant {
-    A(u8, &'static str),
+  A(u8, &'static str),
 }
 
 #[derive(Serialize)]
 struct Nested {
-    a: String,
-    b: char,
+  a: String,
+  b: char,
 }
 
 fn main() {
-    let data = Config {
-        float: (2.18, -1.1),
-        tuple: TupleStruct((), false),
-        map: HashMap::from_iter(vec![(0, '1'), (1, '2'), (3, '5'), (8, '1')]),
-        nested: Nested {
-            a: "Hello from \"RON\"".to_string(),
-            b: 'b',
-        },
-        var: Variant::A(!0, ""),
-        array: vec![(); 3],
-    };
+  let data = Config {
+    float: (2.18, -1.1),
+    tuple: TupleStruct((), false),
+    map: HashMap::from_iter(vec![(0, '1'), (1, '2'), (3, '5'), (8, '1')]),
+    nested: Nested { a: "Hello from \"RON\"".to_string(), b: 'b' },
+    var: Variant::A(!0, ""),
+    array: vec![(); 3],
+  };
 
-    let pretty = PrettyConfig::new()
-        .with_depth_limit(2)
-        .with_separate_tuple_members(true)
-        .with_enumerate_arrays(true);
-    let s = to_string_pretty(&data, pretty).expect("Serialization failed");
+  let pretty = PrettyConfig::new().with_depth_limit(2).with_separate_tuple_members(true).with_enumerate_arrays(true);
+  let s = to_string_pretty(&data, pretty).expect("Serialization failed");
 
-    println!("{}", s);
+  println!("{}", s);
 }
